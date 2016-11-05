88adf613ecb2d4d5f071804448a316857a35bbc0
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-11-05 16:02:11 +0100

    includes propres, Etat de base = start

--- Code/Sources/main.cpp
+++ Code/Sources/main.cpp
@@ -5,34 +5,37 @@
 
 #include <stdio.h>
 #include "mbed.h"
-#include "states.h"
-#include "period.h"
+#include "../Headers/period.h"
+#include "../Headers/states.h"
 
+State state = Start;		// état de base : start
 
 int main ()
 {
 	while(true)
 	{
-		switch(State)
+		switch(state)
 		{
 		case Start:
-			printf("State Start");
+			printf("State Start\n");
 			break;
 		case DeepSleepShort:
-			printf("State DeepSleepShort");
+			printf("State DeepSleepShort\n");
 			break;
 		case DeepSleepLong:
-			printf("State DeepSleepLong");
+			printf("State DeepSleepLong\n");
 			break;
 		case MesPHT:
-			printf("State MesPHT");
+			printf("State MesPHT\n");
 			break;
 		case MesGPS:
-			printf("State MesGPS");
+			printf("State MesGPS\n");
 			break;
 		case Send:
-			printf("State Send");
+			printf("State Send\n");
 			break;
+		default:
+			printf("State Error\n");
 		}
 	}
 }
