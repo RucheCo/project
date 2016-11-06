30512ba8fe8f04a791e439f6fa9f07f4ec4a79ad
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-11-06 13:20:54 +0100

    Ajout des TODO appels de fonctions dans la machine d'état

--- Code/Sources/main.cpp
+++ Code/Sources/main.cpp
@@ -16,26 +16,31 @@ int main ()
 	{
 		switch(state)
 		{
-		case Start:
-			printf("State Start\n");
-			break;
-		case DeepSleepShort:
-			printf("State DeepSleepShort\n");
-			break;
-		case DeepSleepLong:
-			printf("State DeepSleepLong\n");
-			break;
-		case MesPHT:
-			printf("State MesPHT\n");
-			break;
-		case MesGPS:
-			printf("State MesGPS\n");
-			break;
-		case Send:
-			printf("State Send\n");
-			break;
-		default:
-			printf("State Error\n");
+			case Start:
+				printf("State Start\n");
+				break;
+			case DeepSleepShort:
+				printf("State DeepSleepShort\n");
+				//TODO Ajouter appel à standby(...)
+				break;
+			case DeepSleepLong:
+				printf("State DeepSleepLong\n");
+				//TODO Ajouter appel à standby(...)
+				break;
+			case MesPHT:
+				printf("State MesPHT\n");
+				//TODO Ajouter appel à mesPHT(...)
+				break;
+			case MesGPS:
+				printf("State MesGPS\n");
+				//TODO Ajouter appel à mesGPS(...)
+				break;
+			case Send:
+				printf("State Send\n");
+				//TODO Ajouter appel à send(...)
+				break;
+			default:
+				printf("State Error\n");
 		}
 	}
 }
