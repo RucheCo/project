b784ce20b9ee69ee6f5742c0377980855cd6e020
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-11-06 16:42:14 +0100

    Ajout des transitions (TODOs)

--- Code/Sources/main.cpp
+++ Code/Sources/main.cpp
@@ -21,18 +21,31 @@ int main ()
 		{
 			case Start:
 				printf("State Start\n");
+				//TODO Test jour ou nuit
+
+				//TODO SI jour, Etat suivant = DeepSleepShort
+				//TODO SI nuit, Etat suivant = DeepSleepLong
 				break;
 			case DeepSleepShort:
 				printf("State DeepSleepShort\n");
 				//TODO Ajouter appel à standby(...)
+
+				//TODO Etat suivant = Start
 				break;
 			case DeepSleepLong:
 				printf("State DeepSleepLong\n");
 				//TODO Ajouter appel à standby(...)
+
+				//TODO Etat suivant = Start
 				break;
 			case MesPHT:
 				printf("State MesPHT\n");
 				//TODO Ajouter appel à mesPHT(...)
+
+				//TODO Test poids
+
+				//TODO SI poids < 5 Kg, Etat suivant = MesGPS
+				//TODO SINON Etat suivant = Send
 				break;
 			case MesGPS:
 				printf("State MesGPS\n");
@@ -41,9 +54,15 @@ int main ()
 			case Send:
 				printf("State Send\n");
 				//TODO Ajouter appel à send(...)
+
+				//TODO Test jour ou nuit
+
+				//TODO SI jour, Etat suivant = DeepSleepShort
+				//TODO SI nuit, Etat suivant = DeepSleepLong
 				break;
 			default:
 				printf("State Error\n");
-		}
-	}
-}
+				//TODO Etat suivant = Start
+		}//end switch
+	}//end while
+}//end main
