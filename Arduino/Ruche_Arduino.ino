521b9a807df4a7eeb84981afbb89150759e82cde
ValAIRian <behot@et.esiea.fr>
2016-12-12 11:24:53 +0100

    Add files via upload
    Balance

---
+++ Arduino/Ruche_Arduino.ino
@@ -0,0 +1,15 @@
+#include "HX711.h"
+#include "weightScale.h"
+
+
+
+void setup() {
+  weightScale_init();
+  Serial.begin(9600);
+}
+
+void loop() {
+  weightScale_value();
+  Serial.println(weightScale_value());
+
+}
---
+++ Arduino/weightScale.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+//empêche le double appel de ce document
+#ifndef WEIGHTSCALE_H_INCLUDED
+#define WEIGHTSCALE_H_INCLUDED
+
+void weightScale_init();
+double weightScale_value();
+
+
+#endif 
