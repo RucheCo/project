cddf794786fee82b189bc2e3d654054404e94b3a
nadjirou <nba@et.esiea.fr>
2016-12-12 16:53:55 +0100

    Update Ruche_Arduino.ino

--- Arduino/Ruche_Arduino.ino
+++ Arduino/Ruche_Arduino.ino
@@ -1,14 +1,19 @@
 #include "HX711.h"
 #include "weightScale.h"
+#include "tempSonde.h"
 
 
 
-void setup() {
+void setup() 
+{
+  setupSonde();
   weightScale_init();
   Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
+  getTempSonde();
   weightScale_value();
   Serial.println(weightScale_value());
 
