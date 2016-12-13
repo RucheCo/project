4565152ac6d99f452aac7d4b737520c95dc02afd
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-12-13 11:42:53 +0100

    deplacement

---
+++ Arduino/libraries/weightScale/weightScale.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+//empêche le double appel de ce document
+#ifndef WEIGHTSCALE_H_INCLUDED
+#define WEIGHTSCALE_H_INCLUDED
+
+void weightScale_init();
+float weightScale_value();
+
+
+#endif 
