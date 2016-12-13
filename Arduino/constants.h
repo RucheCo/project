6a1e8516920ba97af580dfd0969df519f8be78b5
nadjirou <nba@et.esiea.fr>
2016-12-13 10:49:34 +0100

    Ajout des constantes lever et coucher du soleil

---
+++ Arduino/constants.h
@@ -0,0 +1,23 @@
+/*
+ * constants.h
+ *
+ *  Created on: 6 nov. 2016
+ *      Author: mathieusab
+ */
+
+#ifndef CODE_HEADERS_CONSTANTS_H_
+#define CODE_HEADERS_CONSTANTS_H_
+
+// durée courte de veille en minutes
+#define DEEPSLEEP_SHORT_DURATION (8*60)
+
+// durée courte de veille en minutes
+#define DEEPSLEEP_LONG_DURATION (60*60)
+
+// Heure du lever du soleil min (au solstice 2016) (https://www.kalendrier.com/heure-lever-coucher-soleil/2016/juin/paris.html)
+#define DAY_THRESHOLD (6) //06h00 du matin
+
+// Heure du coucher du soleil max (au solstice 2016) (https://www.kalendrier.com/heure-lever-coucher-soleil/2016/juin/paris.html)
+#define NIGHT_THRESHOLD (22) //22h00
+
+#endif /* CODE_HEADERS_CONSTANTS_H_ */
