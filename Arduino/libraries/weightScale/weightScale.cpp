bb838e15832a51d55cddde0f66e58930f421ab18
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-12-13 11:42:30 +0100

    deplacement

---
+++ Arduino/libraries/weightScale/weightScale.cpp
@@ -0,0 +1,22 @@
+#include "weightScale.h"
+#include "HX711.h"
+
+#define DOUT  3
+#define CLK  2
+
+float calibration_factor = 6300; //-7050 worked for my 440lb max scale setup
+HX711 scale(DOUT, CLK);
+
+
+void weightScale_init()
+{
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0  
+}
+
+float weightScale_value()
+{
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  float pesee =scale.get_units()*0.45359237;
+  return pesee;
+}
