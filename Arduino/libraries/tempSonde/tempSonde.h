ec100fdd8f919b0e421d499446dd5515e9c1aa2a
mathieusab <mathieu.sabarthes@hotmail.fr>
2016-12-13 11:42:06 +0100

    deplacement

---
+++ Arduino/libraries/tempSonde/tempSonde.h
@@ -0,0 +1,7 @@
+#ifndef TEMPSONDE_H_INCLUDED
+#define TEMPSONDE_H_INCLUDED
+
+void setupSonde();
+float getTempSonde();
+
+#endif //TEMPSONDE_H_INCLUDED
--- Arduino/tempSonde.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEMPSONDE_H_INCLUDED
-#define TEMPSONDE_H_INCLUDED
-
-void setupSonde();
-float getTempSonde();
-
-#endif //TEMPSONDE_H_INCLUDED
