0d629511d56df141483cd3be3649f04a373791d5
AntoineDe <delemotte@et.esiea.fr>
2016-12-14 18:48:43 +0100

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,25 +1,33 @@
-#include "HX711.h"
-#include "weightScale.h"
+//#include "HX711.h"
+//#include "weightScale.h"
 #include "tempSonde.h"
-#include "DHT22.h"
+#include "RTC.h"
+//#include "DHT22.h"
 
 
 void setup() 
 {
   setupSonde();
-  weightScale_init();
+  setRtcTime();
+//  weightScale_init();
   Serial.begin(9600);
 }
 
 void loop() 
 {
+  unsigned long int TS = 0;
   float temp = 0.0;
-  float weight = 0.0;
-  float humidity = 0.0;
+  //float weight = 0.0;
+  // float humidity = 0.0;
+  TS = get_timestamp();
   temp = getTempSonde();
-  weight = weightScale_value();
-  humidity = Humidity_value();
-  Serial.println(weight);
+  //weight = weightScale_value();
+  //humidity = Humidity_value();
+  //Serial.println(weight);
+  Serial.print("temperature : ");
   Serial.println(temp);
-  Serial.println(humidity);
+  Serial.print("timestamp : ");
+  Serial.println(TS);
+  Serial.println();
+  //Serial.println(humidity);
 }
