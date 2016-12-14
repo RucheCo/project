dba5f636f9561fb648e0c87299fb6be984c8d3bb
AntoineDe <delemotte@et.esiea.fr>
2016-12-14 18:50:02 +0100

    unsigned long int

--- Arduino/RTC.h
+++ Arduino/RTC.h
@@ -2,15 +2,15 @@
 #define RTC_H_INCLUDE
 
 // fonction qui déclare le RTC
-void setupRTC ()
+void setupRTC();
 
 // fonction pour mettre a jour l'heure du RTC en fonction de la date et heure de la compilation
-void setRtcTime ()
+void setRtcTime();
 
 // fonction pour recupérer le timestamp (nb de secondes depuis 00:00:00 01/01/1970)
-int get_timestamp () 
+unsigned long int get_timestamp();
 
 // fonction qui récupère un binaire ( 1 si jour, 0 si nuit)
-int get_day ()
+int get_day();
 
 #endif //RTC_H_INCLUDE
