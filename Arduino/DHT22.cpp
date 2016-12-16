a6b5cd492e1fd1c77ab4939d4f5525d0fc901ec2
AntoineDe <delemotte@et.esiea.fr>
2016-12-16 12:15:22 +0100

    Tout changer

--- Arduino/DHT22.cpp
+++ Arduino/DHT22.cpp
@@ -1,31 +1,22 @@
-#include "DHT.h"   // Librairie des capteurs DHT
-#define DHTPIN 2    // Changer le pin sur lequel est branché le DHT
-#define DHTTYPE DHT22     // DHT 22  (AM2302)
- 
-DHT dht(DHTPIN, DHTTYPE); 
 
-unsigned int Humidity_value() {
-  // Délai de 2 secondes entre chaque mesure. La lecture prend 250 millisecondes
-  delay(2000);
- 
-  // Lecture du taux d'humidité
-  float h = dht.readHumidity();
-  // Lecture de la température en Celcius
- // float t = dht.readTemperature();
-  // Pour lire la température en Fahrenheit
-//  float f = dht.readTemperature(true);
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+//		100nF entre VDD et GND
+//		R: 1.5 Kohm
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+unsigned int GetHumidity() {
   
-  // Stop le programme et renvoie un message d'erreur si le capteur ne renvoie aucune mesure
-  if (isnan(h)) {
-    Serial.println("Echec de lecture !");
+  // read without samples.
+  byte humidity = 0;
+  if (dht11.read(pinDHT11, &humidity, NULL)) {
+    Serial.print("Read DHT11 failed.");
     return;
   }
- 
-
-  Serial.print("Humidite: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  
-  return((unsigned int)h);
-  
+ return((unsigned int)humidity);
 }
