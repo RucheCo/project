908e5a80a879c7118838a9c92c39a97072f2d9b3
nadjirou <nba@et.esiea.fr>
2016-12-16 16:22:13 +0100

    Ajout du lcd

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,34 +2,43 @@
 //#include "weightScale.h"
 //#include "tempSonde.h"
 //#include "RTC.h"
-#include "DHT11.h"
-
+//#include "DHT11.h"
+#include "lcdI2C.h"
 
 void setup() 
 {
+  Serial.begin(9600);
+  Serial.println("setup begin");
   //setupSonde();
   //setRtcTime();
   //weightScale_init();
-  Serial.begin(9600);
+  setupLcdI2C();
+  Serial.println("setup done");
+
 }
 
 void loop() 
 {
+  Serial.println("main start");
+  
+  char sentenceRow1[16] = "that is how";
+  char sentenceRow2[16] = "it is done";  
   //unsigned long int TS = 0;
   //float temp = 0.0;
   //float weight = 0.0;
-  int humidity = 0;
+  //int humidity = 0;
   //TS = get_timestamp();
   //temp = getTempSonde();
   //weight = weightScale_value();
-  humidity = GetHumidity();
+  //humidity = GetHumidity();
   //Serial.println(weight);
   //Serial.print("temperature : ");
   //Serial.println(temp);
   //Serial.print("timestamp : ");
   //Serial.println(TS);
-  //Serial.println();
-  Serial.println(humidity);
+  //Serial.println();  
+  lcdPrint(sentenceRow1,sentenceRow2);
+  Serial.println("main done");
   
   delay(1000);
 }
---
+++ Arduino/lcdI2C.h
@@ -0,0 +1,7 @@
+#ifndef LCDI2C_H_INCLUDED
+#define LCDI2C_H_INCLUDED
+
+void setupLcdI2C();
+void lcdPrint(char sentenceRow1[], char sentenceRow2[]);
+
+#endif //LCDI2C_H_INCLUDED
